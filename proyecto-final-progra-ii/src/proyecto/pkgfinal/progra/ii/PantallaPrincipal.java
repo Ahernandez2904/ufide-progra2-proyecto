e279d73f60ee665ecdcbe02179076e84acbde5c3
valeriamnz0 <valeria.matamoros@hotmail.com>
2020-04-12 18:31:39 -0700

    RegistroRuta
    Se agrego el registro de ruta con guardado en la carpeta en el proyecto, ademas se agrego el boton de agregar rutas en la pantalla principal

--- proyecto-final-progra-ii/src/proyecto/pkgfinal/progra/ii/PantallaPrincipal.java
+++ proyecto-final-progra-ii/src/proyecto/pkgfinal/progra/ii/PantallaPrincipal.java
@@ -5,7 +5,6 @@ package proyecto.pkgfinal.progra.ii;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Leonardo Vargas
@@ -31,6 +30,7 @@ public class PantallaPrincipal extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        BotonRutas = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -43,6 +43,13 @@ public class PantallaPrincipal extends javax.swing.JFrame {
 
         jLabel1.setText("Bienvenido/a");
 
+        BotonRutas.setText("Agregar Rutas");
+        BotonRutas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonRutasActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -54,8 +61,10 @@ public class PantallaPrincipal extends javax.swing.JFrame {
                         .addComponent(jLabel1))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(137, 137, 137)
-                        .addComponent(jButton1)))
-                .addContainerGap(158, Short.MAX_VALUE))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(BotonRutas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(148, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -64,7 +73,9 @@ public class PantallaPrincipal extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(34, 34, 34)
                 .addComponent(jButton1)
-                .addContainerGap(212, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(BotonRutas)
+                .addContainerGap(171, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -84,6 +95,11 @@ public class PantallaPrincipal extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void BotonRutasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonRutasActionPerformed
+        RegistroRuta rt = new RegistroRuta();   //ruta para Interfaz de Agregar Ruta
+        rt.setVisible(true);
+    }//GEN-LAST:event_BotonRutasActionPerformed
 
     /**
      * @param args the command line arguments
@@ -121,6 +137,7 @@ public class PantallaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BotonRutas;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
